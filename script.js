9807df87c421a5c72c32641bcfa48c9a3ac8e643
De-L3ns <laurensviaene@gmail.com>
2021-09-21 22:47:35 +0200

    started multiple equations logic

--- script.js
+++ script.js
@@ -19,6 +19,10 @@ function divide(numOne, numTwo) { // divide number one by number two
     return numOne / numTwo;
 };
 
+function percentage(numOne, numTwo) { // to do add percentage functionality
+    return nothing;
+}
+
 function operate(operator, numOne, numTwo) { // this function takes an operator and invokes the corresponding function
     if (operator == '+') {
         return add(numOne, numTwo);
@@ -37,7 +41,9 @@ function operate(operator, numOne, numTwo) { // this function takes an operator 
 let startingValue = '0,00';
 let displayValue = '';
 let inputValueOne = [];
-let operator = ''
+let inputValueTwo = [];
+let storedNumber = '';
+let operator = '';
 
 
 const display = document.getElementById('display');
@@ -46,18 +52,33 @@ display.textContent = startingValue;
 const numberButtons = document.querySelectorAll('.number');
 numberButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        inputValueOne.push(button.value);
-        display.textContent = inputValueOne.join('');
+        if (operator) {
+            inputValueTwo.push(button.value);
+            display.textContent = inputValueTwo.join('');
+        } else {
+            inputValueOne.push(button.value);
+            display.textContent = inputValueOne.join('');
+        }
     });
 });
 
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        operator = button.value;
-        console.log(inputValueOne);
-        console.log(operator);
-    })
+        if (operator) {
+            let answer = operate(operator, Number(inputValueOne.join('')), Number(inputValueTwo.join('')))
+            display.textContent = answer;
+            answer = storedNumber;
+            operator = button.value;
+            inputValueOne = storedNumber;
+            inputValueTwo = [];
+        } else {
+            operator = button.value;
+            display.textContent = operator;
+        }
+        
+        
+    });
    
 });
 
@@ -65,5 +86,15 @@ const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', () => {
     display.textContent = startingValue;
     inputValueOne = [];
-    console.log(inputValueOne);
+    inputValueTwo = [];
+    operator = '';
+    storedNumber = '';
+    
+});
+
+
+const equalsButton = document.querySelector('.equals');
+equalsButton.addEventListener('click', () => {
+    let answer = operate(operator, Number(inputValueOne.join('')), Number(inputValueTwo.join('')))
+    display.textContent = answer;
 });
